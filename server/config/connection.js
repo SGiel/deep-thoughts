5868722bbe132e1c1d9c53a10f26c1b83caac39a
Stephanie Giel <stephaniegiel@gmail.com>
2021-05-02 18:39:03 -0700

    added mongoose connection listeners

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,4 +7,12 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
   useFindAndModify: false
 });
 
+mongoose.connection.on('connected', () =>
+  console.log('Connected to MongoDB Endpoint')
+);
+
+mongoose.connection.on('error', (err) =>
+  console.log(`Mongoose default connection error: ${err}`)
+);
+
 module.exports = mongoose.connection;
